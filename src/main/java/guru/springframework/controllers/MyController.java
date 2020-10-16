e695f9c3910730cd44149e55bc2793e24d01a35b
Nikita Kolesnik <nikita.kolesnik@excelian.com>
2020-10-16 16:18:13 +0300

    фыфы

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,6 +1,6 @@
 package guru.springframework.controllers;
 
-import guru.services.GreetingService;
+import guru.springframework.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MyController {
 
-    private GreetingService greetingService;
+    private final GreetingService greetingService;
 
     public MyController(GreetingService greetingService) {
         this.greetingService = greetingService;
